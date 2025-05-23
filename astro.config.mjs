428697deaeb0630324098804678e79f0da2bbe5b
Alexis Gandara <alexis20143@gmail.com>
2025-05-23 00:03:28 -0300

    Mejoras en la sección de contacto y presentación

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'http://localhost:4321',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
